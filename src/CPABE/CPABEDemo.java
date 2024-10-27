98fe2fcbbacb052b9193be682defa2be465cb377
mmsyan <19821237570@163.com>
2024-10-27 11:37:25 +0800

    5 Ciphertext-Policy Attribute Based Encryption的实现。《Ciphertext-Policy Attribute-Based Encryption》。完成了delegate()

--- src/CPABE/CPABEDemo.java
+++ src/CPABE/CPABEDemo.java
@@ -17,6 +17,7 @@ public class CPABEDemo {
     private Element alpha; //Zr
     private Element beta;  //Zr
     private Element h; // h = g^beta;
+    private Element f;
 
     public CPABEDemo(int u) {
         this.universe = u;
@@ -27,7 +28,8 @@ public class CPABEDemo {
         this.g = bp.getG1().newRandomElement().getImmutable(); // g <- G1
         this.alpha = bp.getZr().newRandomElement().getImmutable(); // alpha <- Zr
         this.beta = bp.getZr().newRandomElement().getImmutable(); // beta <- Zr
-        this.h = this.g.powZn(this.beta);
+        this.h = this.g.powZn(this.beta).getImmutable(); // h = g^beta
+        this.f = this.g.powZn(bp.getZr().newOneElement().div(beta)).getImmutable();  // f = g^1/beta
     }
 
     public void keyGeneration(int[] userAttributes, String skFilePath) {
@@ -133,10 +135,41 @@ public class CPABEDemo {
         }
     }
 
+    public void delegate(int[] userAttributes, int[] subSetUserAttributes, String skFilePath, String subSetSKFilePath) {
+        if (!MathUtils.isSubsetUsingSet(subSetUserAttributes, userAttributes)) {
+            System.out.println("需要提供用户属性的子集合！");
+        }
+        Properties skProperties = PropertiesUtils.load(skFilePath);
+        Properties subSetProperties = new Properties();
+
+        // r~ <- Zr; D~ = D*f^(r~)
+        Element rWave = bp.getZr().newRandomElement();
+        String DStr = skProperties.getProperty("D");
+        Element D = bp.getG1().newElementFromBytes(ConversionUtils.String2Bytes(DStr));
+        Element DWave = D.mul(f.powZn(rWave));
+        subSetProperties.setProperty("D", ConversionUtils.bytes2String(DWave.toBytes()));
+
+        for (int k : subSetUserAttributes) {
+            Element rkWave = bp.getZr().newRandomElement().getImmutable();
+
+            String DkStr = skProperties.getProperty("Dj"+k);
+            Element Dk = bp.getG1().newElementFromBytes(ConversionUtils.String2Bytes(DkStr)).getImmutable();
+            Element DkWave = Dk.mul(g.powZn(rWave)).mul((MathUtils.H1(String.valueOf(k), bp)).powZn(rkWave));
+            subSetProperties.setProperty("Dj"+k, ConversionUtils.bytes2String(DkWave.toBytes()));
+
+            String DkPrimeStr = skProperties.getProperty("DjPrime"+k);
+            Element DkPrime = bp.getG1().newElementFromBytes(ConversionUtils.String2Bytes(DkPrimeStr)).getImmutable();
+            Element DkWavePrime = DkPrime.mul(g.powZn(rkWave));
+            subSetProperties.setProperty("DjPrime"+k, ConversionUtils.bytes2String(DkWavePrime.toBytes()));
+        }
+
+        PropertiesUtils.store(subSetProperties, subSetSKFilePath);
+    }
+
     public static void testCase1() {
         //测试文件路径
-        String skFilePath = "src/CPABE/CPABEFile/sk.properties";
-        String ctFilePath = "src/CPABE/CPABEFile/ct.properties";
+        String skFilePath = "src/CPABE/CPABEFile/test1/sk.properties";
+        String ctFilePath = "src/CPABE/CPABEFile/test1/ct.properties";
         System.out.println("\n测试案例1：");
         // 初始化操作，设置属性上限为10
         CPABEDemo cpabeInstance = new CPABEDemo(10);
@@ -159,8 +192,8 @@ public class CPABEDemo {
 
     public static void testCase2() {
         //测试文件路径
-        String skFilePath = "src/CPABE/CPABEFile/sk.properties";
-        String ctFilePath = "src/CPABE/CPABEFile/ct.properties";
+        String skFilePath = "src/CPABE/CPABEFile/test2/sk.properties";
+        String ctFilePath = "src/CPABE/CPABEFile/test2/ct.properties";
         System.out.println("\n测试案例2：");
         // 初始化操作，设置属性上限为10
         CPABEDemo cpabeInstance = new CPABEDemo(20);
@@ -183,8 +216,8 @@ public class CPABEDemo {
 
     public static void testCase3() {
         //测试文件路径
-        String skFilePath = "src/CPABE/CPABEFile/sk.properties";
-        String ctFilePath = "src/CPABE/CPABEFile/ct.properties";
+        String skFilePath = "src/CPABE/CPABEFile/test3/sk.properties";
+        String ctFilePath = "src/CPABE/CPABEFile/test3/ct.properties";
         System.out.println("\n测试案例3：");
         // 初始化操作，设置属性上限为10
         CPABEDemo cpabeInstance = new CPABEDemo(20);
@@ -205,9 +238,42 @@ public class CPABEDemo {
         System.out.println("M_ 是 " + M_);
     }
 
+    public static void testCase4() {
+        //测试文件路径
+        String skFilePath = "src/CPABE/CPABEFile/test4/sk.properties";
+        String subSetSkFilePath = "src/CPABE/CPABEFile/test4/subSetSk.properties";
+        String ctFilePath = "src/CPABE/CPABEFile/test4/ct.properties";
+
+        System.out.println("\n测试案例4：Delegate()函数测试");
+        // 初始化操作，设置属性上限为10
+        CPABEDemo cpabeInstance = new CPABEDemo(10);
+        cpabeInstance.setUp("a.properties");
+
+        // 用户输入自己属性对应的访问控制树来生成密钥
+        int[] userAttributes = new int[]{1, 2, 5};
+        cpabeInstance.keyGeneration(userAttributes, skFilePath);
+
+        // 随机选取Gt上的元素作为消息并打印出来
+        Element M = cpabeInstance.bp.getGT().newRandomElement().getImmutable();
+        System.out.println("M 是 " + M);
+        CPABEAccessTree tree1 = CPABEAccessTree.getInstance1();
+        cpabeInstance.encrypt(tree1, M, ctFilePath);
+
+        Element M_ = cpabeInstance.decrypt(tree1, userAttributes, skFilePath, ctFilePath);
+        System.out.println("M_ 是 " + M_);
+
+        // 子属性集合
+        int[] subSetUserAttributes = new int[]{5, 1};
+        cpabeInstance.delegate(userAttributes, subSetUserAttributes, skFilePath, subSetSkFilePath);
+
+        Element M1_ = cpabeInstance.decrypt(tree1, subSetUserAttributes, subSetSkFilePath, ctFilePath);
+        System.out.println("M1_ 是 " + M1_);
+    }
+
     public static void main(String[] args) {
         testCase1();
         testCase2();
         testCase3();
+        testCase4();
     }
 }
